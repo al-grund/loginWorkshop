6d77da5ff01fd4a12a8e61914f60203d8c6f0070
Pamela Kepa <pkepa@outlook.com>
2019-02-22 11:12:16 +0100

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,7 @@
+var submit=document.getElementById("submit")
+var username=document.getElementById("username")
+var password=document.getElementById("password")
+
+var users = ["first person", "second person", "third person"]
+
+submit.onclick = pasuser(form)
